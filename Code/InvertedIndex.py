f3a7b9199e7e3390305c22430f47efbf8c68fe70
Greg Kapadoukas <gregkap12@gmail.com>
2023-10-22 16:31:04 +0300

    Added ColBERT implementation for Question 3

--- Code/InvertedIndex.py
+++ Code/InvertedIndex.py
@@ -3,44 +3,44 @@ class InvertedIndex:
     def __init__(self):
         self.__content = {}
     
-    def insert(self, word, doc):
-        if word in self.__content:
-            self.__addOccurance(self.__content[word], doc)
+    def insert(self, token, doc):
+        if token in self.__content:
+            self.__addOccurance(self.__content[token], doc)
         else:
-            self.__content[word] = [1, {doc:  1}]
+            self.__content[token] = [1, {doc:  1}]
     
-    def countOccurances(self, word):
+    def countOccurances(self, token):
         count = 0
-        if (word in self.__content.keys()):
-            for doc, n in self.__content[word][1].items():
+        if (token in self.__content.keys()):
+            for doc, n in self.__content[token][1].items():
                 count += n    
             return count
         else:
-            return "Word not found"
+            return "Token not found"
             
-    def delete(self, word):
-        if word in self.__content:
-            del self.__content[word]
-            return f"Word {word} deleted successfully"
+    def delete(self, token):
+        if token in self.__content:
+            del self.__content[token]
+            return f"Token {token} deleted successfully"
         else:
-            return "Word not found"
+            return "Token not found"
     
-    def rename(self, original_word, new_word):
-        if original_word == new_word:
-            return f"Original and new words are the same"
-        elif original_word in self.__content:
+    def rename(self, original_token, new_token):
+        if original_token == new_token:
+            return f"Original and new tokens are the same"
+        elif original_token in self.__content:
             new_inserts = []
-            for doc, frequency in self.__content[original_word][1].items():
+            for doc, frequency in self.__content[original_token][1].items():
                 for i in range(0,frequency):
                     new_inserts.append(doc)
             for doc in new_inserts:
-                self.insert(new_word, doc)
-            self.delete(original_word)
-            return f"Word {original_word} was renamed to {new_word} successfully"
+                self.insert(new_token, doc)
+            self.delete(original_token)
+            return f"Token {original_token} was renamed to {new_token} successfully"
         else:
-            return f"Word {original_word} not found"
+            return f"Token {original_token} not found"
         
-    def getWords(self):
+    def getTokens(self):
         return self.__content.items()
             
     def __addOccurance(self, inverted_list, doc):
@@ -52,8 +52,8 @@ class InvertedIndex:
     
     def __str__(self):
         result = ""
-        for word, data in self.__content.items():
-            result += f"Word: {word} -> n: {data[0]}\n"
+        for token, data in self.__content.items():
+            result += f"Token: {token} -> n: {data[0]}\n"
             for doc, n in data[1].items():
                 result += f"['{str(doc)}', {str(n)}],"
             result = result[:-1] + '\n\n'
